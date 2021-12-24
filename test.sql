62eef240ada0bb08494e19b676f0b9df7465c899
Aamina <81919937+Aami3103@users.noreply.github.com>
2021-12-24 17:02:16 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,4 @@
+create database test;
+use test;
+create table register3(id int primary key auto_increment,name varchar(233),password varchar(233),email varchar(233),sex varchar(233),country varchar(23));
+select * from register3; 
